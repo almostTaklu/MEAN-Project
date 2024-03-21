2e3d5fbff0444a802c3e1c88a483c9a4258620cd
almostTaklu <ach.ganga@outlook.com>
2024-03-21 15:34:53 -0400

    Update blog model and view to display the createdOn date in local time

--- app_api/models/blogs.js
+++ app_api/models/blogs.js
@@ -1,9 +1,24 @@
 var mongoose = require('mongoose');
 
+var getNewDate = function() {
+    return new Date().toLocaleString("en-US", { 
+        timeZone: "America/New_York",
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true});
+}
+
 var blogSchema = new mongoose.Schema({
     blogTitle: String,
     blogText: String,
-    createdOn: { type: Date, default: Date.now }
+    createdOn: { 
+        type: String, 
+        "default": getNewDate 
+    }
 });
 
 mongoose.model('Blog', blogSchema);
